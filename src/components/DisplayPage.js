0990f512a7ed30db33764f318df69f95a68868e9
Sara Bertuldo <sarabertuldo@gmail.com>
2021-03-07 11:11:55 -0600

    cleans up code

--- src/components/DisplayPage.js
+++ src/components/DisplayPage.js
@@ -4,13 +4,12 @@ import { connect } from "react-redux";
 const DisplayPage = ({ pkmn, isCaught, addCatch, deleteCatch }) => {
   return (
     <>
-      {/* <h1>Results</h1> */}
       <div className="results-box">
         <div className="box">
           <h3>{pkmn.name}</h3>
           <div className="pokedex-box">Pokedex No. {pkmn.dex}</div>
           <img src={pkmn.img} />
-          <div className="type-box">{pkmn.type}</div>
+          <div className="type-box ">{pkmn.type}</div>
         </div>
 
         {!isCaught && <button onClick={() => addCatch(pkmn)}> Catch</button>}
